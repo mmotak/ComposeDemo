d905279115db34df5840e13d15024ef269f0e050
mmotak <maciej.motak@gmail.com>
2023-01-19 13:17:13 +0100

    Fix MyTopBar

--- app/src/main/java/pl/mmotak/composedemo/MainActivity.kt
+++ app/src/main/java/pl/mmotak/composedemo/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
--- app/src/main/java/pl/mmotak/composedemo/base/MyTopBar.kt
+++ app/src/main/java/pl/mmotak/composedemo/base/MyTopBar.kt
@@ -2,30 +2,30 @@ package pl.mmotak.composedemo.base
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import pl.mmotak.composedemo.Greeting
 import pl.mmotak.composedemo.ui.theme.ComposeDemoTheme
 
 @Composable
 fun MyTopBar(
     title: String,
+    onBackPressed: (() -> Unit)? = null
 ) {
     TopAppBar(
         title = {
             Text(text = title)
         },
-//        navigationIcon = {
-//            IconButton(onClick = { }) {
-//                Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu Btn")
-//            }
-//        },
+        navigationIcon = if (onBackPressed == null) null else {
+            {
+                IconButton(onClick = onBackPressed) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                }
+            }
+        },
         backgroundColor = MaterialTheme.colors.background,
         contentColor = MaterialTheme.colors.onBackground,
         elevation = 2.dp
@@ -46,8 +46,12 @@ private fun PreviewMyTopBar() {
         Surface(
             color = MaterialTheme.colors.background
         ) {
-            Column  {
+            Column {
                 MyTopBar(title = "PREVIEW !")
+                MyTopBar(
+                    title = "top bar with back arrow",
+                    onBackPressed = {}
+                )
                 Text(text = "body")
             }
         }
